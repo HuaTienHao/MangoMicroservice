5096e2783581fca30f6715ae531b3df2ba681ec5
HuaTienHao <huatienhao095@gmail.com>
2024-09-20 14:02:56 +0700

    Add project files.

---
+++ Mango.Services.ShoppingCartAPI/Service/ProductService.cs
@@ -0,0 +1,28 @@
+﻿using Mango.Services.ShoppingCartAPI.Models.Dto;
+using Mango.Services.ShoppingCartAPI.Service.IService;
+using Newtonsoft.Json;
+
+namespace Mango.Services.ShoppingCartAPI.Service
+{
+    public class ProductService : IProductService
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public ProductService(IHttpClientFactory clientFactory)
+        {
+            _httpClientFactory = clientFactory;
+        }
+        public async Task<IEnumerable<ProductDto>> GetProducts()
+        {
+            var client = _httpClientFactory.CreateClient("Product");
+            var response = await client.GetAsync($"/api/product");
+            var apiContent = await response.Content.ReadAsStringAsync();
+            var apiResponseDto = JsonConvert.DeserializeObject<ResponseDto>(apiContent);
+            if (apiResponseDto.IsSuccess )
+            {
+                return JsonConvert.DeserializeObject<IEnumerable<ProductDto>>(Convert.ToString(apiResponseDto.Result));
+            }
+            return new List<ProductDto>();
+        }
+    }
+}
